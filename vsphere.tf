5a5a666dad5a005d291044b58c66a959268233ac
daniel2korea <daniel2korea@gmail.com>
2023-06-21 17:24:07 +0900

    adf

---
+++ vsphere.tf
@@ -0,0 +1,96 @@
+# Basic configuration without variables
+
+# Define authentification configuration
+provider "vsphere" {
+  # If you use a domain set your login like this "Domain\\User"
+  user           = "linyong@psolab.local"
+  password       = "VMware1!"
+  vsphere_server = "vcsa01.psolab.local"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+#### RETRIEVE DATA INFORMATION ON VCENTER ####
+
+data "vsphere_datacenter" "dc" {
+  name = "vSAN Datacenter"
+}
+
+# If you don't have any resource pools, put "Resources" after cluster name
+data "vsphere_resource_pool" "pool" {
+  name          = "vSAN Cluster/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Retrieve datastore information on vsphere
+data "vsphere_datastore" "datastore" {
+  name          = "vsanDatastore"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Retrieve network information on vsphere
+data "vsphere_network" "network" {
+  name          = "vMGMT-V10-172-20-10-0"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Retrieve template information on vsphere
+data "vsphere_virtual_machine" "template" {
+  name          = "CentOS7.9-minimal"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+#### VM CREATION ####
+
+# Set vm parameters
+resource "vsphere_virtual_machine" "demo" {
+  name             = "vm-one"
+  num_cpus         = 2
+  memory           = 4096
+  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+
+  # Set network parameters
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  # Use a predefined vmware template as main disk
+  disk {
+    label = "vm-one.vmdk"
+    size = "10"
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "vm-one"
+        domain    = "vm-one.psolab.local"
+      }
+
+      network_interface {
+        ipv4_address    = "172.20.10.220"
+        ipv4_netmask    = 24
+        dns_server_list = ["10.109.211.55"]
+      }
+
+      ipv4_gateway = "172.20.10.1"
+    }
+  }
+
+  # Execute script on remote vm after this creation
+  provisioner "remote-exec" {
+    script = "scripts/example-script.sh"
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "VMware1!"
+      host     = vsphere_virtual_machine.demo.default_ip_address 
+    }
+  }
+}
